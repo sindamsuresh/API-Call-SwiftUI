47613fc1a918d7da8c55401f46e2bc049f4b5e91
SureshSindam <sindam.suresh@gmail.com>
2024-01-28 15:02:09 -0600

    Initial Commit

---
+++ SwiftUI API Call/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI API Call
+//
+//  Created by Suresh Sindam on 1/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
